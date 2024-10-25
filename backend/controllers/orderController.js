0f7a23115a90ea1a80616380ade7e970aa201bbd
Vedantvb17 <147984304+Vedantvb17@users.noreply.github.com>
2024-10-25 14:58:24 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //config variables
 const currency = "inr";
-const frontend_URL = 'http://localhost:5173';
+const frontend_URL = 'http://localhost:5174';
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
